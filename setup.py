704693c0db78adea28a335c196d92dea5f93f9c8
Sayan the researcher <sm6881164@gmail.com>
2026-02-28 23:37:48 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ setup(
     ],
     classifiers=[
         "Programming Language :: Python :: 3",
-        "License :: OSI Approved :: Apache License",
+        "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
     ],
     python_requires='>=3.6',
